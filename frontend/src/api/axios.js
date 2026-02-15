b7e9973d31b3dfeb81df6a73e392cc3f2f3f610f
AbhinavS0007 <abhinavsachan635@gmail.com>
2026-02-15 22:30:45 +0530

    Prepare frontend for deployment

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,16 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8000/api",
-});
-
-// attach token automatically
-instance.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default instance;
